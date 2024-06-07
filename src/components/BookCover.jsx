6eecd004f9acf6cb94eebf577296b8b4dc48118b
graceK-10 <kipagrace@gmail.com>
2024-06-07 18:55:21 +0200

    cover page image

--- src/components/BookCover.jsx
+++ src/components/BookCover.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import HTMLFlipBook from 'react-pageflip';
+import Page from './Page';
+import Cover from './bookPages/Cover/Cover';
+import coverImg from "../assets/recipe-book-cover-img.jpeg";
 
-const BookCover = () => {
-  return (
-    <div>BookCover</div>
-  )
-}
+function BookCover(props) {
+    return (
+        <HTMLFlipBook width={600} height={700} showCover="true">
+          <Page number={1}>
+          <Cover coverImg={coverImg} title="My Recipe Book" />
+          </Page>
+          <Page number={2}>
+          <div className="demoPage text-white text-3xl">Page 2</div>
+          </Page>
+          <Page number={3}>
+          <div className="demoPage text-white text-3xl">Page 3</div>
+          </Page>
+          <Page number={4}>
+          <div className="demoPage text-white text-3xl">Page 4</div>
+          </Page>       
+        </HTMLFlipBook>
+    );
+} 
 
-export default BookCover
+export default BookCover;
